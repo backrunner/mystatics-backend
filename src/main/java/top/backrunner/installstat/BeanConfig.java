c4a4710d3e07a5805b65201c898c1de06264c7a0
BackRunner <liaoyp126@126.com>
2019-11-19 17:42:49 +0800

    init project

---
+++ src/main/java/top/backrunner/installstat/BeanConfig.java
@@ -0,0 +1,21 @@
+package top.backrunner.installstat;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters((HttpMessageConverter<?>) fastConverter);
+    }
+}
---
+++ src/test/java/top/backrunner/installstat/InstallstatApplicationTests.java
@@ -0,0 +1,13 @@
+package top.backrunner.installstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstallstatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
